4dc00da81e351229a91e947041203f86e50413c2
Juno Lee <lejuno80@gmail.com>
2018-02-26 20:42:02 -0600

    Challenge 3

--- string.js
+++ string.js
@@ -7,3 +7,9 @@ console.log("numberOfSandwhiches is", numOfSandwhiches);
 var name = "juno";
 var string = "hello, " + name + " how are you doing today?";
 console.log(string);
+
+//challenge #3
+var DNA = "GCAT";
+var RNA = DNA[0]+ DNA[1] + DNA[2] + "U";
+console.log("RNA:", RNA);
+
