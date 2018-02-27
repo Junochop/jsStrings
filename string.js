1dd255d1bf5e0e6dcfb8d6e91569c492bbeb36b1
Juno Lee <lejuno80@gmail.com>
2018-02-26 20:31:22 -0600

    challenge 2

--- string.js
+++ string.js
@@ -2,3 +2,8 @@
 
 var numOfSandwhiches = 4 * 2;
 console.log("numberOfSandwhiches is", numOfSandwhiches);
+
+//challenge #2
+var name = "juno";
+var string = "hello, " + name + " how are you doing today?";
+console.log(string);
